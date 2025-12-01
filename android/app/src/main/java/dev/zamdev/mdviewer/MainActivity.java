9bfe08ced0a6a4fe6f78c320af7f2b93ff4e47c2
zamdevio <me@zamdev.dev>
2025-12-01 17:41:35 +0800

    add: Capacitor Android Added

---
+++ android/app/src/main/java/dev/zamdev/mdviewer/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.zamdev.mdviewer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.zamdev.mdviewer',
+  appName: 'MD Viewer',
+  webDir: 'out'
+};
+
+export default config;
