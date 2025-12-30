625e97f781864561a6d227812e267af4c9250983
zamdevio <me@zamdev.dev>
2025-12-30 19:04:31 +0800

    remove: Capacitor and Android platform support

--- android/app/src/main/java/dev/zamdev/mdviewer/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package dev.zamdev.mdviewer;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'dev.zamdev.mdviewer',
-  appName: 'MD Viewer',
-  webDir: 'out'
-};
-
-export default config;
